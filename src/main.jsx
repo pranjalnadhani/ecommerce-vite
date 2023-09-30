aa3762e741b6311a98c22cd2d748de023288f6c2
Pranjal Nadhani <pranjalnadhani@gmail.com>
2023-09-30 10:53:34 +0530

    fix globals.css import into main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
 import { ProductDetailsPage } from "./pages/ProductDetails";
+import "./globals.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
